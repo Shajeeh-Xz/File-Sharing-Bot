6be6722ec267a55800ca9a9f5b29b5c4dbf92795
Shajeeh-Xz <89102187+Shajeeh-Xz@users.noreply.github.com>
2024-04-08 11:16:05 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -88,7 +88,7 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("👁️EPISODE CHANNEL 📂", callback_data = "about")
+                    InlineKeyboardButton("👁️ EPISODE CHANNEL 📂", callback_data = "about")
                     
                 ]
             ]
